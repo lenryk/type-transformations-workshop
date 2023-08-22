6e8a532a417b7b69ae01d43bafcf0b51cdf3b64c
Sam <lenry101@gmail.com>
2023-08-22 14:08:56 +0100

    exercise 13

--- src/02-unions-and-indexing/13-get-array-value.problem.ts
+++ src/02-unions-and-indexing/13-get-array-value.problem.ts
@@ -1,9 +1,11 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-const fruits = ["apple", "banana", "orange"];
+// need to add as const or else they will all be inferred as string
+const fruits = ["apple", "banana", "orange"] as const;
 
-type AppleOrBanana = unknown;
-type Fruit = unknown;
+// since it's an array we pass index numbers instead of property names in the union
+type AppleOrBanana = typeof fruits[0 | 1];
+type Fruit = typeof fruits[number];
 
 type tests = [
   Expect<Equal<AppleOrBanana, "apple" | "banana">>,
