4a6eb40881bb32e3bf1d8bdd1ddf539b1d1c4c06
Sam <lenry101@gmail.com>
2023-08-22 10:24:40 +0100

    exercise 05

--- src/02-unions-and-indexing/05-terminology.problem.ts
+++ src/02-unions-and-indexing/05-terminology.problem.ts
@@ -8,6 +8,10 @@
  * Which is which?
  */
 
+// discriminated union
+// each option needs something in common which in this case is the type
+// by doing a conditional on this type property (as it is present on all)
+// TS will only autocomplete other props from that type
 type A =
   | {
       type: "a";
@@ -22,8 +26,11 @@ type A =
       c: string;
     };
 
+// union type
+// the syntax is T1 OR T2 OR T3
 type B = "a" | "b" | "c";
 
+// enum type - the keyword gives it away
 enum C {
   A = "a",
   B = "b",
