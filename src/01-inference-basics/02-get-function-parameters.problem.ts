337b440502cf3fa1dd6b4ff630f30d4c59a61a35
Sam <lenry101@gmail.com>
2023-08-22 09:41:49 +0100

    exercise 02

--- src/01-inference-basics/02-get-function-parameters.problem.ts
+++ src/01-inference-basics/02-get-function-parameters.problem.ts
@@ -11,7 +11,11 @@ const makeQuery = (
   },
 ) => {};
 
-type MakeQueryParameters = unknown;
+// we use the Parameters utility to return a tuple of the types for the parameters in makeQuery
+// this is ideal for functions that you don't have control of
+type MakeQueryParameters = Parameters<typeof makeQuery>;
+// we can pick just one of the parameters e.g. just the 1st one (url which is type string)
+type MakeQueryParametersOne = MakeQueryParameters[0]
 
 type tests = [
   Expect<
