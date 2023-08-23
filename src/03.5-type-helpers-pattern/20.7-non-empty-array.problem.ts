087c7b9438a93c57bad39af2d3a3ed04a823541a
Sam <lenry101@gmail.com>
2023-08-23 14:34:54 +0100

    exercise 20.7

--- src/03.5-type-helpers-pattern/20.7-non-empty-array.problem.ts
+++ src/03.5-type-helpers-pattern/20.7-non-empty-array.problem.ts
@@ -1,4 +1,7 @@
-type NonEmptyArray = unknown;
+// since the array cant be empty it needs to be a tuple so we can define a length
+// we pass T inside our tuple for the first arg
+// the second arg is a rest param so we can have unlimited length array of type T
+type NonEmptyArray<T> = [T, ...Array<T>];
 
 export const makeEnum = (values: NonEmptyArray<string>) => {};
 
