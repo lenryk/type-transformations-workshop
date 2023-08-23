4d556f6fa5ba142b86217fb37262d602497a7973
Sam <lenry101@gmail.com>
2023-08-23 11:13:29 +0100

    exercise 18

--- src/03-template-literals/18-template-literals-in-object-keys.problem.ts
+++ src/03-template-literals/18-template-literals-in-object-keys.problem.ts
@@ -2,7 +2,9 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type TemplateLiteralKey = `${"user" | "post" | "comment"}${"Id" | "Name"}`;
 
-type ObjectOfKeys = unknown;
+// using Record<> allows us to create a structure for our object
+// We use the TemplateLiteralKey as all the props and set them all to string type
+type ObjectOfKeys = Record<TemplateLiteralKey, string>;
 
 type tests = [
   Expect<
