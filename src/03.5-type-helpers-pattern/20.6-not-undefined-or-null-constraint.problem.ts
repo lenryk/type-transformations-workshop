ad572ca2309e8b1a19e12d86202e4e5207966d8b
Sam <lenry101@gmail.com>
2023-08-23 14:28:04 +0100

    exercise 20.6

--- src/03.5-type-helpers-pattern/20.6-not-undefined-or-null-constraint.problem.ts
+++ src/03.5-type-helpers-pattern/20.6-not-undefined-or-null-constraint.problem.ts
@@ -1,4 +1,5 @@
-export type Maybe<T> = T | null | undefined;
+// the empty object represents any type that is not null or undefined
+export type Maybe<T extends {}> = T | null | undefined;
 
 type tests = [
   // @ts-expect-error
