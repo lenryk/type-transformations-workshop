b7548b91d5a8c27e33cf156e3945753603ba5d61
Sam <lenry101@gmail.com>
2023-08-23 10:50:00 +0100

    exercise 15

--- src/03-template-literals/15-extract-with-template-literals.problem.ts
+++ src/03-template-literals/15-extract-with-template-literals.problem.ts
@@ -2,6 +2,8 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type Routes = "/users" | "/users/:id" | "/posts" | "/posts/:id";
 
-type DynamicRoutes = unknown;
+// by using Extract we only allow strings that match the format
+// this is like regex expression for extracting template literals
+type DynamicRoutes = Extract<Routes, `${string}:${string}`>;
 
 type tests = [Expect<Equal<DynamicRoutes, "/users/:id" | "/posts/:id">>];
