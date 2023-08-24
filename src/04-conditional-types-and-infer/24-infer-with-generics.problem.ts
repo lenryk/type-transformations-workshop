9a3937afe3949cf7988bf7cc4bc4725ebc819b94
Sam <lenry101@gmail.com>
2023-08-24 11:17:14 +0100

    exercise 24

--- src/04-conditional-types-and-infer/24-infer-with-generics.problem.ts
+++ src/04-conditional-types-and-infer/24-infer-with-generics.problem.ts
@@ -14,6 +14,10 @@ type Example = MyComplexInterface<
   { x: 12; y: 14 }
 >;
 
-type GetPoint<T> = unknown;
+// by extending MyComplexInterface we have a constraint that checks if we have all the args
+// we can also grab the type of any argument by inferring that argument
+// we can then reference that type in the conditional to return it
+// we could also infer every other argument
+type GetPoint<T> = T extends MyComplexInterface<any, any, any, infer TPoint> ? TPoint : never;
 
 type tests = [Expect<Equal<GetPoint<Example>, { x: 12; y: 14 }>>];
