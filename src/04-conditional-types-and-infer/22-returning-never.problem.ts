0b3a42ebf743ab86c600bb2f6a6cb659bf3a0694
Sam <lenry101@gmail.com>
2023-08-24 10:15:02 +0100

    exercise 22

--- src/04-conditional-types-and-infer/22-returning-never.problem.ts
+++ src/04-conditional-types-and-infer/22-returning-never.problem.ts
@@ -1,6 +1,10 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type YouSayGoodbyeAndISayHello<T> = T extends "hello" ? "goodbye" : "hello";
+// using nested ternaries we can add more if else logic
+// this check uses a union to check if the T input is "hello" or "goodbye"
+// if so we return our "hello" or "goodbye" logic
+// else we just return never
+type YouSayGoodbyeAndISayHello<T> = T extends "hello" | "goodbye" ? T extends "hello" ? "goodbye" : "hello" : never
 
 type tests = [
   Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
