fb02c6718c8f8eb82c6884e32d34109f15dad51b
Sam <lenry101@gmail.com>
2023-08-24 12:20:22 +0100

    exercise 28

--- src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
+++ src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
@@ -2,6 +2,12 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type Fruit = "apple" | "banana" | "orange";
 
-type AppleOrBanana = Fruit extends "apple" | "banana" ? Fruit : never;
+// distributive conditional types
+// https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+// convert this to a new type that uses generics
+type GetAppleOrBanana<T> = T extends "apple" | "banana" ? T : never;
+
+// reference that generic with the original Fruit type to get our new type
+type AppleOrBanana = GetAppleOrBanana<Fruit>;
 
 type tests = [Expect<Equal<AppleOrBanana, "apple" | "banana">>];
