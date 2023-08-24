69b4009da5b1815def184ed55f32665bc674bf9f
Sam <lenry101@gmail.com>
2023-08-24 10:00:23 +0100

    exercise 21

--- src/04-conditional-types-and-infer/21-conditional-types.problem.ts
+++ src/04-conditional-types-and-infer/21-conditional-types.problem.ts
@@ -1,6 +1,9 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type YouSayGoodbyeAndISayHello = unknown;
+// with the extends keyword we are checking if it can be compared to hello
+// if so we return a literal type of "goodbye"
+// anything that is not type "hello" returns "hello"
+type YouSayGoodbyeAndISayHello<T> = T extends "hello" ? "goodbye" : "hello";
 
 type tests = [
   Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
