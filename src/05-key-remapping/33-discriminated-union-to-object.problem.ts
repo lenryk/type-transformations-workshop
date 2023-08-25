496ca698af165d343e48f0ae3f39b3f120b83e15
Sam <lenry101@gmail.com>
2023-08-25 11:34:07 +0100

    exercise 33

--- src/05-key-remapping/33-discriminated-union-to-object.problem.ts
+++ src/05-key-remapping/33-discriminated-union-to-object.problem.ts
@@ -12,7 +12,12 @@ type Route =
   | { route: "/admin"; search: {} }
   | { route: "/admin/users"; search: {} };
 
-type RoutesObject = unknown;
+// we map through all the unions of Route and access the route prop for the key name (as each has one)
+// we then access the object which is K and use index props to access the search object
+// in this case K is the object in the union so we can access props with K["search"]
+type RoutesObject = {
+  [K in Route as K["route"]]: K["search"]
+};
 
 type tests = [
   Expect<
