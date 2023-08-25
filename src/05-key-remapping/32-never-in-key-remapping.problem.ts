2229db887777df68df840d70a0f2d8d3195e02a6
Sam <lenry101@gmail.com>
2023-08-25 11:28:19 +0100

    exercise 32

--- src/05-key-remapping/32-never-in-key-remapping.problem.ts
+++ src/05-key-remapping/32-never-in-key-remapping.problem.ts
@@ -8,7 +8,13 @@ interface Example {
   groupId: string;
 }
 
-type OnlyIdKeys<T> = unknown;
+// here we map our Example interface and grab all the keys into K
+// we also add a conditional that checks if the key contains id or Id
+// if so we keep the key and just return the normal value
+// if it doesn't we remove it from the type with setting it to never
+type OnlyIdKeys<T> = {
+  [K in keyof T as K extends `${string}${"id" | "Id"}` ? K : never]: T[K]
+};
 
 type tests = [
   Expect<
