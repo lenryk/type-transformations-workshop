c51a1079174490651d5c8849d872718270c6363c
Sam <lenry101@gmail.com>
2023-08-25 11:50:02 +0100

    exercise 34

--- src/05-key-remapping/34-object-to-union-of-tuples.problem.ts
+++ src/05-key-remapping/34-object-to-union-of-tuples.problem.ts
@@ -6,9 +6,11 @@ interface Values {
   lastName: string;
 }
 
+// the map is just a normal type map but returns the key as a tuple [keyname, type]
+// to turn this into a union of tuples we have to add on [keyof Values]
 type ValuesAsUnionOfTuples = {
   [K in keyof Values]: [K, Values[K]];
-};
+}[keyof Values];
 
 type tests = [
   Expect<
