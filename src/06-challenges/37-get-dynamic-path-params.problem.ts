4d0ffb3f4f1a03b5afecc50c257348e5f59b17c8
Sam <lenry101@gmail.com>
2023-08-31 22:36:52 +0100

    exercise 37

--- src/06-challenges/37-get-dynamic-path-params.problem.ts
+++ src/06-challenges/37-get-dynamic-path-params.problem.ts
@@ -1,10 +1,17 @@
 import { Equal, Expect } from "../helpers/type-utils";
+import { S } from "ts-toolbelt";
 
 type UserPath = "/users/:id";
 
 type UserOrganisationPath = "/users/:id/organisations/:organisationId";
 
-type ExtractPathParams = unknown;
+// add a constraint so we can only pass strings
+type ExtractPathParams<T extends string> = {
+  // split the string via / so we can access the inner values
+  // if the individual section has a colon at the start then return that
+  // if it doesn't match exclude it
+  [K in S.Split<T, "/">[number] as K extends `:${infer P}` ? P : never]: string
+}
 
 type tests = [
   Expect<Equal<ExtractPathParams<UserPath>, { id: string }>>,
