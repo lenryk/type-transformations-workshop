a104d1f4188ba9580d7246672032754a7619ee2f
Sam <lenry101@gmail.com>
2023-09-01 21:28:31 +0100

    exercise 40

--- src/06-challenges/40-deep-partial.problem.ts
+++ src/06-challenges/40-deep-partial.problem.ts
@@ -1,6 +1,11 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type DeepPartial<T> = unknown;
+// check if T is an array
+type DeepPartial<T> = T extends Array<infer U>
+  // if so then return an array with the inferred type (eventually this will not be an array and follow the 2nd path)
+  ? Array<DeepPartial<U>>
+  // if not an array grab the name as the key and call it recursively
+  : { [K in keyof T]?: DeepPartial<T[K]> };
 
 type MyType = {
   a: string;
